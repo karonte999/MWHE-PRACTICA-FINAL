4687f1cc756d927338a52133faeb25c1b14ac583
karonte999 <karonteisalive@gmail.com>
2025-01-03 19:34:23 +0100

    JUAN_ADD03_cam_1

---
+++ index.js
@@ -0,0 +1,20 @@
+//GALERÍA
+
+
+
+
+document.querySelectorAll('.gallery-item').forEach(item => {
+    item.addEventListener('click', () => {
+        alert('Has hecho clic en una imagen!');
+    });
+});
+
+
+
+
+
+
+// ACCORDION FAQ
+
+
+
