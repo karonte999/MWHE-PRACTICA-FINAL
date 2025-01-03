128385dddaff6a53e0d820b35c95d6d808432779
grisel-pacsi <griselepacsi@gmail.com>
2025-01-03 12:05:06 -0500

    GRISEL_INTENTO_GSAP
    Intento gsap installation

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 //GALERÍA
 
-document.querySelectorAll('.gallery-item').forEach(item => {
-    item.addEventListener('click', () => {
-        alert('Has hecho clic en una imagen!');
-    });
+// document.querySelectorAll('.gallery-item').forEach(item => {
+//     item.addEventListener('click', () => {
+//         alert('Has hecho clic en una imagen!');
+//     });
+// });
+
+
+
+
+// NO FUNCIONA 
+
+// use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    // gsap code here!
+    gsap.registerPluging(ScrollTrigger);
+
+    gsap.to('.square', {
+        rotation:360,
+        duration: 5,
+        ScrollTrigger: {
+            trigger: '.square',
+            markers: true,
+            scrub: true
+        }
+    })
+
 });
+  
+
+
